d636376cfd09cd64207da1127b8f522ba6f4b3d4
Hugo Sträng <hugo.strang2@gmail.com>
2025-09-03 21:11:45 +0200

    new scripts

--- experiments/utils.py
+++ experiments/utils.py
@@ -15,6 +15,7 @@ import os
 from typing import Callable
 import logging
 from logging import Logger
+import matplotlib.pyplot as plt 
 
 # =======
 # Logging
@@ -324,3 +325,44 @@ def load_arff_as_distance_matrix(path, metric="euclidean", scale=False):
     D = squareform(pdist(X, metric=metric))
 
     return D, X, y
+
+
+# ========
+# Plotting
+# ========
+
+def get_silhouette_plot_data(labels, scores, n_clusters, ub_samples):
+
+    data = {
+        i : {} for i in range(1, n_clusters + 1)
+    }
+
+    y_lower = 10
+    for i in data.keys():
+
+        indices = np.where(labels == i)[0]
+        cluster_silhouettes = scores[indices]
+        cluster_ub_values = ub_samples[indices]
+
+        # Get sorted order of silhouette values
+        sorted_order = np.argsort(cluster_silhouettes)
+
+        sorted_silhouettes = cluster_silhouettes[sorted_order]
+        sorted_ub_values = cluster_ub_values[sorted_order]
+
+        size_cluster_i = sorted_silhouettes.shape[0]
+        y_upper = y_lower + size_cluster_i
+
+        color = plt.cm.viridis(float(i) / n_clusters)
+
+        data[i]['y_lower'] = y_lower
+        data[i]['y_upper'] = y_upper 
+        data[i]['sorted_silhouettes'] = sorted_silhouettes
+        data[i]['color'] = color
+        data[i]['sorted_ub_values'] = sorted_ub_values
+        data[i]['size_cluster_i'] = size_cluster_i
+
+        # update y_lower
+        y_lower = y_upper + 10
+    
+    return data 
