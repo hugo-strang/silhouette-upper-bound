9440278abf286755a9c1ba1a352ab9b07d136f44
Hugo Sträng <hugo.strang2@gmail.com>
2026-01-04 17:39:38 +0100

    distance between cluster centers

--- experiments/table_asw_synthetic_data.py
+++ experiments/table_asw_synthetic_data.py
@@ -3,6 +3,7 @@ This file generates a table comparing empirical ASW values with the upper bound 
 """
 
 from sklearn.datasets import make_blobs
+from scipy.spatial.distance import pdist
 import utils
 
 
@@ -79,23 +80,56 @@ def table(caseparams: list):
         print(line)
 
 
+def cluster_centers(params):
+    n_samples, n_features, centers, cluster_std = params
+    
+    # Generate synthetic data
+    X, y, cluster_centers = make_blobs(
+        n_samples=n_samples,
+        n_features=n_features,
+        centers=centers,
+        cluster_std=cluster_std,
+        random_state=0,
+        return_centers=True
+    )
+
+    # Compute all pairwise distances
+    dists = pdist(cluster_centers, metric="euclidean")
+
+    # Average distance between centers
+    avg_dist = dists.mean()
+
+    print(f"params: {params}")
+    print("cluster_centers shape:", cluster_centers.shape)
+    print(f"min inter-center distance: {dists.min()}")
+    print(f"max inter-center distance: {dists.max()}")
+    print(f"max - min inter-center distance: {dists.max() - dists.min()}")
+
+    print("\naverage inter-center distance:", f"{avg_dist:.1f}")
+
+    return avg_dist
+
+
+
 if __name__ == "__main__":
 
     # n_samples, n_features, n_centers, cluster_std
     case1params = (400, 64, 5, 6)
     case2params = (400, 64, 2, 2)
     case3params = (400, 128, 7, 3)
-    case5params = (1000, 300, 5, 2)
-    case6params = (10000, 32, 20, 2)
-    case7params = (10000, 1024, 20, 4)
+    case4params = (1000, 300, 5, 2)
+    case5params = (10000, 32, 20, 2)
+    case6params = (10000, 1024, 20, 4)
 
     table(
         [
             case1params,
             case2params,
             case3params,
+            case4params,
             case5params,
             case6params,
-            case7params,
         ]
     )
+
+    cluster_centers(case6params)
