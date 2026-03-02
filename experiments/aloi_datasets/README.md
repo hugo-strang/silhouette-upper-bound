fdffdeedc87555930befa89c6b0c52b3131b8f5b
Hugo Sträng <hugo.strang2@gmail.com>
2026-03-02 14:53:37 +0100

    refactor large scale aloi experiments

--- experiments/aloi_datasets/README.md
+++ experiments/aloi_datasets/README.md
@@ -1,6 +1,6 @@
 # ALOI Datasets
 
-This directory contains experiments on data derived from the ALOI dataset. These datasets are publicly available and can be downloaded from https://zenodo.org/records/6355684. 
+This directory contains experiments on data derived from the ALOI dataset. These datasets are publicly available and can be downloaded from https://zenodo.org/records/6355684. In particular, we explore the datasets ALOI HSB 5x5x5 and 14x6x6.
 
 ## Setup & Generation
 
--- experiments/aloi_datasets/common.py
+++ experiments/aloi_datasets/common.py
@@ -8,9 +8,10 @@ from sklearn.metrics import (
 )
 from silhouette_upper_bound import upper_bound
 from collections import Counter
+import time
 
 
-def get_df_sampled(dataset_name):
+def get_df_sampled(dataset_name, class_size: int = 100):
     """
     Help function to load dataframe.
     """
@@ -31,7 +32,7 @@ def get_df_sampled(dataset_name):
     # 3. sample n instances from each class
     df_sampled = (
         df.groupby("class", group_keys=False)
-        .apply(lambda g: g.sample(n=100, random_state=42))
+        .apply(lambda g: g.sample(n=class_size, random_state=42))
         .reset_index(drop=True)
     )
 
@@ -73,3 +74,69 @@ def eval(D, y, cluster_labels, ub):
     print(
         f"\n{len(cluster_sizes)}&{min_cluster_size}&{ari:.3f}&{ami:.3f}&{asw:.3f}&{ub:.3f}&{((ub - asw)/ub):.3f}&{uba:.3f}&{((uba - asw)/uba):.3f}"
     )
+
+
+def eval_ub_runtime(D, y, cluster_labels):
+    """
+    Help function to print evaluation data including runtimes for upper bound compuations.
+    """
+
+    # ===== Eval on cluster labels =====
+    cluster_sizes = list(Counter(cluster_labels).values())
+    n_clusters = len(cluster_sizes)
+    min_cluster_size = min(cluster_sizes)
+
+    # silhouette
+    silh_samples = silhouette_samples(X=D, labels=cluster_labels, metric="precomputed")
+    asw = np.mean(silh_samples)
+
+    # AMI and ARI
+    ari = adjusted_rand_score(cluster_labels, y)
+    ami = adjusted_mutual_info_score(cluster_labels, y)
+
+    # ===== Global UB =====
+    start = time.perf_counter()
+    ub_global = upper_bound(D)
+    ub_global_rt = (time.perf_counter() - start) / 1000
+    ub_global_wcre = (ub_global - asw) / ub_global
+
+    # ===== Constrained UB =====
+    start = time.perf_counter()
+    ub_constrained = upper_bound(D, min_cluster_size)
+    ub_constrained_rt = (time.perf_counter() - start) / 1000
+    ub_constrained_wcre = (ub_constrained - asw) / ub_constrained
+
+    # ===== Display =====
+
+    measurements = [
+        n_clusters,
+        min_cluster_size,
+        ari,
+        ami,
+        asw,
+        ub_global,
+        ub_global_wcre,
+        ub_global_rt,
+        ub_constrained,
+        ub_constrained_wcre,
+        ub_constrained_rt,
+    ]
+
+    headers = [
+        "Clusters",
+        "Min|C_I|",
+        "ARI",
+        "AMI",
+        "ASW",
+        "UB",
+        "WCRE",
+        "RT",
+        "UB_C",
+        "UB_C_WCRE",
+        "UB_C_RT",
+    ]
+
+    w = 10
+    print("".join(f"{h:<{w}}" for h in headers))
+    print("-" * (w * len(headers)))
+    print("".join(f"{m:<{w}.3f}" for m in measurements))
